542d5a7522ef0084690d21713bed79e51955cb7c
Jake Plimack <jake.plimack@gmail.com>
2014-08-26 09:49:43 -0600

    notify apache that it has a new site to serve

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -26,13 +26,26 @@ template "#{node['apache']['dir']}/sites-available/apt_repo.conf" do
   source "apt_repo.conf.erb"
   mode 0644
   variables(
-      :repo_dir => node['reprepro']['repo_dir']
+    :repo_dir => node['reprepro']['repo_dir']
   )
 end
 
-apache_site "apt_repo.conf"
+apache_site "apt_repo.conf" do
+  notifies :reload, "service[apache2]", :delayed
+end
 
 apache_site "000-default" do
   enable false
+  notifies :reload, "service[apache2]", :delayed
 end
 
+service "apache2" do
+  supports [:reload, :restart, :start, :stop, :status]
+  action [:enable, :start]
+  case node["platform"]
+  when "centos","redhat","fedora"
+    service_name "httpd"
+  else
+    service_name "apache2"
+  end
+end
