eb6e1a17a47d6948beafa5a636f6f3ccddb6b4fd
Tim Smith <tsmith84@gmail.com>
2015-11-20 17:08:24 -0800

    Remove Opscode / Chef ownership

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'reprepro'
-maintainer 'Chef Software, Inc.'
-maintainer_email 'cookbooks@chef.io'
+maintainer 'Tim Smith'
+maintainer_email 'tsmith@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures reprepro for an apt repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
