6bc691cac68e667795663c06ac9b08cf3b1017db
Tim Smith <tsmith@chef.io>
2017-05-31 11:38:55 -0700

    Use SPDX standard license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'reprepro'
 maintainer 'Tim Smith'
 maintainer_email 'tsmith@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs/Configures reprepro for an apt repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.4.2'
