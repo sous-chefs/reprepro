5e08c85ce6eeb5b0840bac25d6cf3cb97408f0c0
Jason Field <jason@avon-lea.co.uk>
2019-08-05 18:52:13 +0100

    fixed reprepro

--- attributes/default.rb
+++ attributes/default.rb
@@ -34,7 +34,7 @@ default['reprepro']['disable_databag'] = false
 
 default['reprepro']['nginx']['listen_port'] = 80
 default['reprepro']['nginx']['ssl'] = false
-default['reprepro']['nginx']['ssl_certificate'] = "#{node['nginx']['dir']}/ssl/#{node['reprepro']['fqdn']}.crt"
-default['reprepro']['nginx']['ssl_certificate_key'] = "#{node['nginx']['dir']}/ssl/#{node['reprepro']['fqdn']}.key"
+# default['reprepro']['nginx']['ssl_certificate'] = "#{node['nginx']['dir']}/ssl/#{node['reprepro']['fqdn']}.crt"
+# default['reprepro']['nginx']['ssl_certificate_key'] = "#{node['nginx']['dir']}/ssl/#{node['reprepro']['fqdn']}.key"
 
 default['reprepro']['server'] = 'apache'
