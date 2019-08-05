9b8eb12dd8be0d584a89105f59a7dad116ded2d7
Jason Field <jason@avon-lea.co.uk>
2019-08-05 18:26:04 +0100

    nginx dir for testing

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -1,5 +1,7 @@
 apt_update
 
 package 'apt-transport-https' if platform_family?('debian')
+# Set for testing purposes
+node.default['nginx']['dir'] = '/etc/nginx'
 
 include_recipe 'reprepro::default'
